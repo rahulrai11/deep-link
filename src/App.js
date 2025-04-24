db4014a3ba075e068d8ce5aab10d689462b3cf2e
Rahul Rai <rahul.rai1@ltts.com>
2025-04-24 14:54:43 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -6,32 +6,32 @@ import HTMLPage from './deeplink/HTMLPage';
 import Onbording from './Onbording';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    // <>
-    //   <BrowserRouter>
-    //     <Routes>
-    //       <Route path="/" element={<Onbording />} />
-    //       <Route path="/app" element={<HTMLPage />} />
-    //       <Route path="/app/*" element={<DeepNotFound />} />
-    //     </Routes>
-    //   </BrowserRouter>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Onbording />} />
+          <Route path="/app" element={<HTMLPage />} />
+          <Route path="/app/*" element={<DeepNotFound />} />
+        </Routes>
+      </BrowserRouter>
 
-    // </>
+    </>
 
   )
 }
