1a3904f9fa9c4152d3d25ced1e8ebe794de6abb1
Rahul Rai <rahul.rai1@ltts.com>
2025-04-25 18:49:45 +0530

    fix

--- src/deeplink/DeepNotFound.js
+++ src/deeplink/DeepNotFound.js
@@ -98,8 +98,8 @@ function DeepNotFound() {
                     just hit the store and start your journey
                 </a>
             </p>
-            
-            <ReactPlayer
+
+            {/* <ReactPlayer
                 className='react-player'
                 playing={true}
                 loop
@@ -107,6 +107,17 @@ function DeepNotFound() {
                 playsInline={true} // Ensures proper playback on iOS Safari
                 url='https://qrcodeblobstorage.blob.core.windows.net/qrcodecontainer/bastians/bastian.mp4'
                 width='100%'
+            /> */}
+
+            <ReactPlayer
+                className="react-player"
+                playing={true}
+                loop
+                muted
+                playsInline={true}
+                controls={true} // Allows users to manually start the video if autoplay fails
+                url="https://qrcodeblobstorage.blob.core.windows.net/qrcodecontainer/bastians/bastian.mp4"
+                width="100%"
             />
 
         </div>
