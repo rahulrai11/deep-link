b52b4e375fc6d12a04e59cf116ade0274a472404
Rahul Rai <rahul.rai1@ltts.com>
2025-04-30 09:53:22 +0530

    fix

--- src/Onbording.js
+++ src/Onbording.js
@@ -28,12 +28,44 @@ function Onbording() {
             }, 500);
         }
 
-        openApp();
+        // openApp();
     }, [])
 
+    const reDirectToStore = () => {
+        const appStoreURL = "https://apps.apple.com/in/app/bastian-customer-parts-portal/id6473263265";
+        window.location.replace(appStoreURL);
+    }
+
     return (
-        <div>
-            <p>cheching...Off we go! If Bastian already chilling on your device, we'll take you there. If not, no worries—we'll zip you to the store in a flash!.</p>
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            textAlign: "center"
+        }}>
+            <p style={{ marginTop: "10px", fontSize: "18px", width: "80%" }}>
+                To view the Bastian Customer Parts Portal, please<a href="#"
+                    onClick={(event) => {
+                        event.preventDefault(); // Prevent default navigation
+                        reDirectToStore(); // Call the function when clicked
+                    }}
+                    style={{ textDecoration: "underline", color: "blue" }}>
+                    download
+                </a>
+                the app in the App Store
+            </p>
+
+            <ReactPlayer
+                className='react-player'
+                playing={true}
+                loop
+                muted
+                playsInline={true} // Ensures proper playback on iOS Safari
+                url='https://qrcodeblobstorage.blob.core.windows.net/qrcodecontainer/bastians/bastian.mp4'
+                width='100%'
+            />
         </div>
     )
 }
